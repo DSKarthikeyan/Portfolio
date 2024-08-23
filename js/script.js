19169c9b4e39a1ad7239c1efa6a20f23160bac3e
Karthikeyan D S <karthikrider92@gmail.com>
2024-08-23 19:27:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -160,24 +160,3 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
-
-let options = {
-  startAngle: -1.55,
-  size: 150,
-  value: 0.85,
-  fill: { gradient: ['#a445b2', '#fa4299'] }
-}
-$(".circle .bar").circleProgress(options).on('circle-animation-progress',
-  function (event, progress, stepValue) {
-    $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
-  });
-$(".js .bar").circleProgress({
-  value: 0.70
-});
-$(".node .bar").circleProgress({
-  value: 0.90
-});
-$(".react .bar").circleProgress({
-  value: 0.60
-});
-
